97c275f32d9583634138335e9590cfdf2b2790a1
farruxabdumannobov890-design <farruxabdumannobov890@gmail.com>
2026-08-24 09:41:07 +0500

    Add: 13) 2 xonali son: o‘nlik va birlik [farruxabdumannobov890-design]

---
+++ lms_kod_topshiriqlari/01-Asoslar/05-Sonlar__int_va_float___butun_va_kasr_sonlar/13-13__2_xonali_son__o_nlik_va_birlik.py
@@ -0,0 +1,7 @@
+# 10–99 oralig'ida butun son kiriting.
+# O'nlik raqam: son // 10
+# Birlik raqam: son % 10
+# "Tens: <t>" va "Ones: <o>" ko'rinishida ikki qatorda chiqaring.
+son = int(input())
+print("Tens:", son // 10 )
+print("Ones:", son % 10)
